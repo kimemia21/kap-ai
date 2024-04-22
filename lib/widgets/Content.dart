2ae578ecd562b3090d404589a2a73edce2afd94c
kimemia21 <bobbymbogo71@gmail.com>
2024-04-22 15:09:11 +0300

    homepage layout

--- lib/widgets/Content.dart
+++ lib/widgets/Content.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class content extends StatefulWidget {
   const content({super.key});
@@ -11,8 +15,31 @@ class _contentState extends State<content> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(top: 20),
       padding: EdgeInsets.all(10),
-      child:Container() ,
+      child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Text(
+                    "How may i help \n you today?",
+                    style: GoogleFonts.poppins(fontSize: 34),
+                  )),
+              Container(
+                height: 300,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(10),
+                    color: Colors.black),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
