2e595aff96af682670d2167e0168c95bcdf8fe44
kimemia21 <bobbymbogo71@gmail.com>
2024-04-22 12:43:31 +0300

    Using the normal chat bot

--- lib/widgets/ChatBot.dart
+++ lib/widgets/ChatBot.dart
@@ -1,10 +1,12 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ikchatbot/ikchatbot.dart';
-
+import 'package:simple_icons/simple_icons.dart';
+import 'package:dialogflow_grpc/dialogflow_grpc.dart';
 
 class ChatBot extends StatelessWidget {
   const ChatBot({super.key});
+
 
   // This widget is the root of your application.
   @override
@@ -30,7 +32,7 @@ class ChatBot extends StatelessWidget {
       smtpPort: 587,
       //Settings to your system Configurations
       sendIcon: const Icon(Icons.send, color: Colors.black),
-      userIcon: const Icon(Icons.animation, color: Colors.white),
+      userIcon: const Icon(SimpleIcons.andela, color: Colors.white),
       botIcon: const Icon(Icons.android, color: Colors.white),
       botChatColor: Color.fromARGB(255, 104, 0, 101),
       delayBot: 100,
@@ -38,8 +40,8 @@ class ChatBot extends StatelessWidget {
       delayResponse: 1,
       userChatColor: Color.fromARGB(255, 228, 208, 208),
       waitingTime: 1,
-      keywords: ["Hello","World"],
-      responses:["hello","World"],
+      keywords: ["one", "two"],
+      responses: ["hello", "World"],
       backgroundColor: Colors.white,
       backgroundImage:
           'https://news.uchicago.edu/sites/default/files/styles/full_width/public/images/2023-07/Human%20aware%20AI%20hero.png?itok=BL5ceKsp',
